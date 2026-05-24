301c5c5e6f138341abb87024b0b0ad57ddb9e404
Michele Dargam <michele.dargam@gmail.com>
2026-05-24 04:03:43 -0300

    fix prisma vercel build

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 // Previne múltiplas instâncias em desenvolvimento (Hot Reload do Next.js)
